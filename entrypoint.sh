74a15454712ad7a6c3f312707884b00cc4834fb4
Scott Garner <scott@j38.net>
2023-12-19 21:42:19 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-fluidsynth -is -a alsa -m alsa_seq -r 48000 -c 3 -z 480 -C 0 -R 0 -g 2 /usr/share/sounds/sf2/FluidR3_GM.sf2
+fluidsynth -is -a alsa -m alsa_seq -p "FluidSynth" -g 1 /usr/share/sounds/sf2/FluidR3_GM.sf2
