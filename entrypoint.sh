fa7d9adf512207d06791d1e7ced7972a6d318f01
Scott Garner <scott@j38.net>
2023-12-19 22:09:41 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-fluidsynth -is -a alsa -m alsa_seq -p "FluidSynth" -g 1 /usr/share/sounds/sf2/FluidR3_GM.sf2
+fluidsynth -is -a alsa -m alsa_seq -o "midi.autoconnect=1" -p "FluidSynth" -C0 -R0 -g 1 /usr/share/sounds/sf2/FluidR3_GM.sf2
